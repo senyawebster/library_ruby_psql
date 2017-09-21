2cc1296a09f83e52edb95dce92d92cd35204ac46
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-09-21 13:41:42 -0700

    clean up author_specs

--- lib/author.rb
+++ lib/author.rb
@@ -7,19 +7,19 @@ class Author
   end
 
   def self.all
-  returned_authors = DB.exec("SELECT * FROM authors;")
-  authors = []
-  returned_authors.each() do |author|
-    id = author.fetch('id').to_i
-    author = author.fetch('author')
-    authors.push(Author.new({:id => id, :author => author}))
+    returned_authors = DB.exec("SELECT * FROM authors;")
+    authors = []
+    returned_authors.each() do |author|
+      id = author.fetch('id').to_i
+      author = author.fetch('author')
+      authors.push(Author.new({:id => id, :author => author}))
+    end
+    authors
   end
-  authors
-end
 
-def save
-  result = DB.exec("INSERT INTO authors (author) VALUES ('#{@author}') RETURNING id;")
-  @id = result.first().fetch('id').to_i()
-end
+  def save
+    result = DB.exec("INSERT INTO authors (author) VALUES ('#{@author}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
 
 end
